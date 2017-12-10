46c081dc9063afd900b908499075f98b2de8292c
YachenQin <yachenqin@gmail.com>
2017-12-10 00:00:18 -0600

    problem4.1

--- problem4.R
+++ problem4.R
@@ -1,5 +1,10 @@
+#generate die
 die=c(1,2,3,4,5,6)
+#set probablity of each outcomes
 p=c(0.34,0.18,0.05,0.18,0.13,0.12)
+#Generate 10,000 samples 
 Xvalues=sample(die,100000,replace=TRUE,p)
+#generate frequency histogram
 hist(Xvalues,freq=F)
+#generate frequency-density graph
 plot(density(Xvalues))
